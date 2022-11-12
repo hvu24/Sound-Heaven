9e1eac3a2b426f9d5770dabfd2b0d9532b4c6e0b
Anthony Hung Vu <hvu24@horizon.csueastbay.edu>
2022-11-12 13:16:21 -0800

    finished get all songs

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -1,1 +1,20 @@
 const express = require('express')
+
+const { Song } = require('../../db/models');
+
+
+
+const router = express.Router();
+
+
+router.get('/', async (req, res) => {
+
+    const songs = await Song.findAll();
+
+    if (!songs) throw new NotFoundError('No songs found');
+
+    return res.json(songs);
+});
+
+
+module.exports = router;
