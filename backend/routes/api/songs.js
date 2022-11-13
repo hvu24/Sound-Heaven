f6c1460c752e507a314c47e3580653989a63803d
Anthony Hung Vu <hvu24@horizon.csueastbay.edu>
2022-11-12 18:10:55 -0800

    finished get songs by current user

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const { requireAuth } = require('../../utils/auth');
 const { Song } = require('../../db/models');
 
 
@@ -16,6 +16,19 @@ router.get('/', async (req, res) => {
     return res.json(songs);
 });
 
+router.get('/current', requireAuth, async (req, res) => {
+    const { user } = req;
 
+    const songs = await Song.findAll({
+        where: {
+            artistId: user.id
+        }
+    })
+
+    if (songs.length === 0) throw new Error('No songs found');
+
+    return res.json(songs);
+}
+);
 
 module.exports = router;
