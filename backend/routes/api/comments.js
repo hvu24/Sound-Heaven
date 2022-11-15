9d516a4629e72e47fc483df4715a44837ff60427
Anthony Hung Vu <hvu24@horizon.csueastbay.edu>
2022-11-14 23:34:55 -0800

    finished edit comment

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -1,10 +1,51 @@
 const express = require('express')
 const { requireAuth } = require('../../utils/auth');
 const { Song, Artist, Album, Comment, Playlist, User } = require('../../db/models');
-
-
+const { handleValidationErrors } = require('../../utils/validation');
+const { check } = require('express-validator');
 
 const router = express.Router();
+
+const validateComment = [
+    check('body')
+        .exists({ checkFalsy: true })
+        .withMessage('Comment body text is required.'),
+    handleValidationErrors
+];
+
+//edit comment
+router.put('/:commentId', requireAuth, validateComment, async (req, res, next) => {
+    const { body } = req.body
+    const { commentId } = req.params
+    const { user } = req;
+
+    const comment = await Comment.findOne({
+        where: {
+            id: commentId
+        }
+    })
+
+    if (comment) {
+        if (comment.userId !== user.id) {
+            const err = new Error('User not authorized to edit comment.');
+            err.status = 403;
+            err.title = 'User not authorized to edit comment.';
+            err.errors = ['User not authorized to edit comment.'];
+            return next(err)
+        } else {
+            comment.set({
+                body
+            })
+            return res.json(comment)
+        }
+    } else {
+        const err = new Error("Comment couldn't be found.");
+        err.status = 404;
+        err.title = "Comment couldn't be found.";
+        err.errors = ["Comment couldn't be found."];
+        return next(err)
+    }
+})
 
 //delete comment by current user
 router.delete('/:commentId', requireAuth, async (req, res, next) => {
