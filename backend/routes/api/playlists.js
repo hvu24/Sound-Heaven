e1c3982698ad915c69f64a645832d97a83b3410a
Anthony Hung Vu <hvu24@horizon.csueastbay.edu>
2022-11-17 01:39:24 -0800

    edited song and playlist models to exclude PlaylistId & SongId, added aliasing to get details of playlist by id

--- backend/routes/api/playlists.js
+++ backend/routes/api/playlists.js
@@ -116,6 +116,7 @@ router.get('/:playlistId', async (req, res, next) => {
         },
         include: {
             model: Song,
+            as: 'Songs',
             through: { //through with empty attributes array excludes the SongPlaylist object
                 attributes: [] //add attributes to array to include properties from SongPlaylist object
             },
